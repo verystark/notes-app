7d34759ce17a0b7214079507e3d99aec0e7dfa56
verystark <tuomaspaloheimo05@gmail.com>
2024-10-11 19:05:58 +0300

    renamed:    dist/assets/index-D2e_Q4wG.js -> dist/assets/index-DpnbwmHr.js #	modified:   dist/index.html #	modified:   src/services/notes.js #	modified:   vite.config.js #

--- src/services/notes.js
+++ src/services/notes.js
@@ -3,7 +3,7 @@ const baseUrl = '/api/notes'
 
 const getAll = () => {
   const request = axios.get(baseUrl)
-  return request.then(response => response.data.concat(nonExisting))
+  return request.then(response => response.data)
 }
 
 const create = newObject => {
